d14d599e42b0eaa8f413378342026d00f0f874ce
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-03-28 00:20:45 +0500

    optimization commit Tv_App

--- app/src/main/java/com/example/myapplication/Activity/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity2.java
@@ -12,16 +12,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.Adapter.TeacherDataRecyclerAdapter;
+import com.example.myapplication.BottomMenu.BottomMenuFragment;
 import com.example.myapplication.Fragments.Bottom_Sheet_Fragment;
 import com.example.myapplication.Model.DataModel;
 import com.example.myapplication.R;
 import com.example.myapplication.ViewModel.TeacherDataViewModel;
+import com.example.myapplication.databinding.ActivityMain2Binding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,52 +36,16 @@ import java.util.List;
 
 
 public class MainActivity2 extends AppCompatActivity {
-        private RecyclerView itemRecycler;
-        private TeacherDataRecyclerAdapter recyclerAdapter;
-        private List<DataModel> teacherDataList;
-        private DatabaseReference databaseReference;
 
-        private TextView uploadText;
-        private String dbKey;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main2);
+    private ActivityMain2Binding binding;
 
-            itemRecycler = findViewById(R.id.recyclerView);
-            recyclerAdapter = new TeacherDataRecyclerAdapter(new ArrayList<>());
-            itemRecycler.setAdapter(recyclerAdapter);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-
-            // Get showKey value
-            String androidId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
-            DatabaseReference keyReference = FirebaseDatabase.getInstance().getReference("Tv_keys").child(androidId);
-            keyReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    dbKey = snapshot.child("EnteredKey").getValue(String.class);
-                    if (dbKey != null) {
-                        // Initialize ViewModel and observe data changes
-                        TeacherDataViewModel viewModel = new ViewModelProvider(MainActivity2.this).get(TeacherDataViewModel.class);
-                        viewModel.getTeacherDataList(dbKey).observe(MainActivity2.this, new Observer<List<DataModel>>() {
-                            @Override
-                            public void onChanged(List<DataModel> newDataList) {
-                                recyclerAdapter.setData(newDataList);
-                            }
-                        });
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    Log.e("Firebase", "Error: " + error.getMessage());
-                }
-            });
-
-
-            GridLayoutManager layoutManager = new GridLayoutManager(this, 4);
-            itemRecycler.setLayoutManager(layoutManager);
-        }
+    }
 
 
     @Override
@@ -86,17 +53,40 @@ public class MainActivity2 extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.action_bar_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode){
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                // Handle left button press
+                return true;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                // Handle right button press
+                return true;
+            case KeyEvent.KEYCODE_DPAD_UP:
+                // Handle up button press
+                return true;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                // Handle down button press
+                return true;
+            default:
+                return super.onKeyDown(keyCode, event);
+        }
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.keyIcon){
+        if (id == R.id.keyIcon) {
             Toast.makeText(this, "CLicked", Toast.LENGTH_SHORT).show();
             // Get the FragmentManager
             FragmentManager fragmentManager = getSupportFragmentManager();
 
             // Begin the fragment transaction
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            Bottom_Sheet_Fragment fragment = new Bottom_Sheet_Fragment();
+            //  Bottom_Sheet_Fragment fragment = new Bottom_Sheet_Fragment();
+            BottomMenuFragment fragment = new BottomMenuFragment();
             fragmentTransaction.replace(R.id.fragment_container, fragment);
             fragmentTransaction.addToBackStack(null); // Add the fragment to the back stack
             fragmentTransaction.commit();
---
+++ app/src/main/java/com/example/myapplication/BottomMenu/BottomMenuFragment.java
@@ -0,0 +1,40 @@
+package com.example.myapplication.BottomMenu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.myapplication.Fragments.Bottom_Sheet_Fragment;
+import com.example.myapplication.databinding.BottomMenuLayoutBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomMenuFragment extends BottomSheetDialogFragment {
+
+    public BottomMenuFragment() {
+    }
+
+
+    private BottomMenuLayoutBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = BottomMenuLayoutBinding.inflate(inflater,container,false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        binding.dismissBtn.setOnClickListener(v -> {
+            dismiss();
+        });
+    }
+}
